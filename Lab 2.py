1482428252ba52736b76032d7359a5a8c12f3d06
Norlon <nsibug@flemingcollege.ca>
2025-01-21 12:22:47 -0500

    create a new script

---
+++ Lab 2.py
@@ -0,0 +1,33 @@
+def main():
+
+    about_me = { 
+    "Studentinfo": [
+        {
+            "Name": "Norlon",
+            "Age": 36,
+            "Student_id": 10312229
+            }
+    ],
+    "Pizza": [
+        {
+            "Meat": "Peperoni",
+            "Seafood": "Shrimp",
+            "Fruit": "Pineapple",
+            "Vegies": ["Mushroom", "Bell pepper"]
+        }
+    ],
+    "Movie": {
+     "Genres": {
+            "Film Genres1": "Comedy",
+            "Film Genres2": "Romance",
+            "Film Genres3": "Action",
+            "Film Genres4": ["Horror"]
+        },
+        "Movie Title": {
+            "List1": "The Hangover",
+            "List2": "Titanic",
+            "List3": "Black Hawk Down",
+            "List4": ["World War Z"]
+        }
+    }
+}    
